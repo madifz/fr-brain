5d636b93b29b4e56e17e05c5857b4bde58252895
Danial Faizsal <m.d.faizsal@gmail.com>
2022-01-27 21:35:00 +0800

    Update components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particle from "./components/Particle/Particle";
 
 function App() {
   return (
     <div className="App">
+      <Particle />
       <Navigation />
       <Logo />
-      {/*
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition />*/}
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
