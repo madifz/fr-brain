d16bb8c810dc2d6a8c3dcb81db80f3fa3b87390c
Danial Faizsal <m.d.faizsal@gmail.com>
2022-02-01 09:51:47 +0800

    updated image detection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React, { Component } from "react";
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
+import Clarifai from "clarifai";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Particle from "./components/Particle/Particle";
+import FaceRecognition from "./components/FaceRecogniton/FaceRecognition";
 
-function App() {
-  return (
-    <div className="App">
-      <Particle />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: "3bbc68f4e2804322b369b6520bfc7fb7",
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particle />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
