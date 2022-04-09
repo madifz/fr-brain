b0e6c8989425886dee61ab46cd67632d14c6aeb6
Danial Faizsal <m.d.faizsal@gmail.com>
2022-04-09 23:02:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -78,9 +78,22 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then((response) => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch((err) => console.log(err));
   };
 
@@ -107,6 +120,7 @@ class App extends Component {
             <Logo />
             <Rank
               name={this.state.user.name}
+              entries={this.state.user.entries}
               onRouteChange={this.onRouteChange}
             />
             <ImageLinkForm
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -7,7 +7,6 @@ const Rank = ({ name, entries }) => {
         {`${name}, your current entry count is...`}
       </div>
       <div className="white f1">
-        {"#5"}
         {entries}
       </div>
     </div>
